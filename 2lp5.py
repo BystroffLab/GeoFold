9dbec1ac3c4fd64685f19028142d98d12720b8b9
Benjamin David Walcott <benjamin.walcott@gmail.com>
2016-03-21 18:29:18 -0400

    Moved scripts to helpscripts

--- 2lp5.py
+++
@@ -1,5 +0,0 @@
-import commands
-for n in range(300,410,10):
-  cmd = "python rungeofold.py 2lp5.%s.par walcob"%(n)
-  status,output = commands.getstatusoutput(cmd)
-  print("%s: %s"%(status,output))
