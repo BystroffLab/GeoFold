63058b4b0ecff104cd6f2b1304026cfd70203e26
JulieSanw <wenyinsan@pelorus-162.dynamic.rpi.edu>
2016-04-21 14:52:19 -0400

    ..

--- test_mpunfoldsim.py
+++ test_mpunfoldsim.py
@@ -10,28 +10,31 @@ from get_cpu_num import get_cpu_num
 import os
 import sys
 
-def test_mpunfoldsim(argFile):
-	MPIRUN = get_mpirun()
-	#nproc = get_cpu_num()
-	# shell("make > make.log")
-	"""
-	omegaRange = omegaRange
-	tmpDir = arg['tmpDir']
-	LName = arg['LName']
-	nn = arg['nn']
-	paramFilename = arg['paramFilename'] 
-    thermal = arg['thermal']
-    htmlTmp = arg['htmlTmp']
-    doIT = arg['doIT']
-    """
-    this_dir = os.getcwd()
-    command = this_dir + "/py_hello_world.py" #+ " " + "argFile"
+def test_mpunfoldsim():
+        MPIRUN = get_mpirun()
+        #nproc = get_cpu_num()
+        # shell("make > make.log")
+        """
+        omegaRange = omegaRange
+        tmpDir = arg['tmpDir']
+        LName = arg['LName']
+        nn = arg['nn']
+        paramFilename = arg['paramFilename'] 
+        thermal = arg['thermal']
+        htmlTmp = arg['htmlTmp']
+        doIT = arg['doIT']
+        """
+        this_dir = os.getcwd()
+        command = this_dir + "/py_hello_world.py" #+ " " + "argFile"
 	status, output = launch(command, runcmd=MPIRUN, "hosts", nproc=5, pipe=False)
 	# Save the output in a log file
 	f = open("run.log", "w")
 	f.seek(0)
 	f.write(str(output))
 	f.close()
+
+if __name__ == '__main__':
+        test_mpunfoldsim()
 
 
 
