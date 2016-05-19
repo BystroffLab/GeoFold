5fcf97b7760dc524fb48cfee05f18182ab3ba1ac
JulieSanw <wenyinsan@astrolabe-226.dynamic.rpi.edu>
2016-05-19 14:15:49 -0400

    …

--- mpipypi.py
+++ mpipypi.py
@@ -25,7 +25,7 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 # I will change these parameters for the performance table
-slice_size = 750000
+slice_size = 1000000
 total_slices = 40
 
 start = time.time()
