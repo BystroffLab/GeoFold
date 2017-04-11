4aeb5a10b7f441185ca7c5f376ba864b39fb0ed9
Benjamin David Walcott <benjamin.walcott@gmail.com>
2017-04-11 15:58:00 -0400

    hx2cij.py can now be run on its own instead of just through import

--- hx2cij.py
+++ hx2cij.py
@@ -1,3 +1,5 @@
+import argparse
+
 def max_let(let1, let2):
     if 'R' in [let1,let2]:
         return 'R'
@@ -24,7 +26,7 @@ def hx2cij(hxfile,cijfile,output):
     #find all contacts in cij where both residues have hx data
     contacts = []
     for [i,j,x] in cij_data:
-        is_hx = False
+        is_hx = [False,False]
         for [k, let] in hx_data:
             if i == k:
                 is_hx[0] = True
@@ -41,7 +43,15 @@ def hx2cij(hxfile,cijfile,output):
     out.close()
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description="Convert a list of early, intermediate, and late folding residues into an age-based contact map")
+    #hxfile
+    parser.add_argument('-hx',metavar='file.hx',dest='hxfile',nargs=1,help='file containing the list of residues')
+    #cijfile
+    parser.add_argument('-cij',metavar='file.cij',dest='cijfile',nargs=1,help='file containing protein contact map')
+    #output
+    parser.add_argument('-o',metavar='file.hxcij',dest='output',nargs=1,help='Output contact map')
+    args=parser.parse_args()
+    hx2cij(args.hxfile[0],args.cijfile[0],args.output[0])
 
 if __name__ == '__main__':
     main()
