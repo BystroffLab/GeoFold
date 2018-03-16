779844a4e8dff68bca825a4c41ba71850db2106d
Benjamin David Walcott <benjamin.walcott@gmail.com>
2018-03-16 14:24:01 -0400

    Added protocol to read parameters file

--- rungeofold_p3.py
+++ rungeofold_p3.py
@@ -78,11 +78,114 @@ class MultiOut(object):
         for f in self.handles:
             f.write(s)
 
+def readParameters(parFile):
+    output = {}
+    try:
+        parIn = open(parFile)
+    except IOError:
+        sys.exit("parameter file %s could not be opened"%(parFile))
+    lines = [line for line in parIn]
+    parIn.close()
+    # pdbcode
+    line = [x for x in lines if x.split()[0] == 'PDBCODE']
+    if len(line) == 0: 
+        sys.exit("ERROR: Parameters file missing PDBCODE")
+    else:
+        output['pdbcode'] = line[-1].split()[1]
+    # thermal
+    line = [x for x in lines if x.split()[0] == 'THERMAL']
+    if len(line) == 0: 
+        output['thermal'] = False
+    else:
+        output['thermal'] = line[-1].split()[1] == '1'
+    # keyword
+    line = [x for x in lines if x.split()[0] == 'KEYWORD']
+    if len(line) == 0:
+        output['keyword'] = ''
+    else:
+        output['keyword'] = line[-1].split()[1]
+    # chain
+    line = [x for x in lines if x.split()[0] == 'CHAIN']
+    if len(line) == 0:
+        sys.exit("ERROR: Parameters file missing CHAIN")
+    else:
+        output['chain'] = line[-1].split()[1]
+    # lname
+    line = [x for x in lines if x.split()[0] == 'LNAME']
+    if len(line) == 0:
+        output['lname'] = "%s.%s"%(output['keyword'],output['pdbcode'])
+    else:
+        output['lname'] = line[-1].split()[1]
+    # strip lname of suspicious characters
+    output['lname'] = output['lname'].strip("/*{}[]!@#$%^&();:<>,?\\~\"\'")
+    # email
+    line = [x for x in lines if x.split()[0] == 'EMAIL']
+    if len(line) == 0:
+        output['email'] = None
+    else:
+        output['email'] = line[-1].split()[1]
+    # oname
+    line = [x for x in lines if x.split()[0] == 'ONAME']
+    if len(line) == 0:
+        output['rerun'] = False
+    else:
+        output['rerun'] = True
+        output['oname'] = output['lname']
+    output['uname'] = '%s%s%s'%(output['pdbcode'],output['chain'],output['lname'])
+    # reducing
+    line = [x for x in lines if x.split()[0] == 'REDUCING']
+    if len(line) == 0: 
+        output['reducing'] = False
+    else:
+        output['reducing'] = line[-1].split()[1] == '1'
+    # debug
+    line = [x for x in lines if x.split()[0] == 'DEBUG']
+    if len(line) == 0: 
+        output['debug'] = False
+    else:
+        output['debug'] = line[-1].split()[1] == '1'
+    # barrelmoves
+    line = [x for x in lines if x.split()[0] == 'BARRELMOVES']
+    if len(line) == 0: 
+        output['barrelmoves'] = False
+    else:
+        output['barrelmoves'] = line[-1].split()[1] == '1'
+    # OMEGA
+    line = [x for x in lines if x.split()[0] == 'OMEGA']
+    if len(line) == 0:
+        output['omega'] = None
+    else:
+        output['omega'] = line[-1].split()[1]
+    # ORANGE
+    line = [x for x in lines if x.split()[0] == 'ORANGE']
+    if len(line) == 0:
+        if output['omega'] is None:
+            sys.exit("ERROR: ORANGE and OMEGA missing in parameters file")
+        else:
+            output['orange'] = [output['omega']]
+    else:
+        output['orange'] = line[-1].split()[1:]
+    # REDO
+    line = [x for x in lines if x.split()[0] == 'REDO']
+    if len(line) == 0:
+        output['redo'] = None
+    else:
+        output['redo'] = line[-1].split()[1]
+    
+    return output
+
 def main():
+    # parser
     parser = argparse.ArgumentParser()
+    # config file
+    parser.add_argument("-conf",default="default.conf",help="config file to use for GeoFold")
+    # parameters file
+    parser.add_argument("-par",help="parameters file for this run of GeoFold")
+    args = parser.parse_args()
     # commandline parameters
     
     # parameters
+    parameters = readParameters(args.par)
     # getchain
     # renumber
     # 3to1
